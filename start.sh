d3915f7c1e9ab7edfac847a2d65f16c75c3c9b97
whatbee <gabe.bf@gmail.com>
2025-11-08 20:22:36 +0000

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-kill -s SIGINT $(pgrep python)
-#kill -9 $(ps -A | grep python | awk '{print $1}')
+# kill -s SIGINT $(pgrep python)
+kill -s SIGINT $(ps -A | grep python | awk '{print $1}')
 rm nohup.out
 echo '' >> log.txt
-(nohup uv run app.py 2> /dev/null &)
+(nohup uv run app.py >> log.txt &)
