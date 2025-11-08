0569431297377ab45c15db5c2fbc3ac87ab90afd
whatbee <gabe.bf@gmail.com>
2025-11-08 20:03:38 +0000

    fix test script

--- test_curl.sh
+++ test_curl.sh
@@ -1,4 +1,3 @@
 bash start.sh
-sleep 10
-curl -X POST -H "Content-Type: application/json" -d '{"event":"sleep_tracking_started"}' http://192.168.1.117:5000/sleep
-echo $?
+sleep 5
+curl -q -X POST -H "Content-Type: application/json" -d '{"event":"sleep_tracking_started"}' http://192.168.1.117:5000/sleep | grep OK
