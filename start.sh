61ce07f0e506116ef40748a509530bd7baa0ff63
Gabriel <gabe.bf@gmail.com>
2025-11-16 16:45:15 +0000

    edit bash scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# kill -s SIGINT $(pgrep python)
+#kill -s SIGINT $(pgrep python)
 kill -s SIGINT $(ps -A | grep python | awk '{print $1}')
-rm nohup.out
+#rm nohup.out
 echo '' >> log.txt
 (nohup uv run app.py >> log.txt &)
