0f09e230aadc40ef5fbe8449415c90ae37ce3d7a
whatbee <gabe.bf@gmail.com>
2025-12-12 22:18:10 +0000

    update WLED backup

--- WLED_backup/backup_cfg.py
+++ WLED_backup/backup_cfg.py
@@ -6,7 +6,7 @@ sys.path.append(str(Path(__file__).parents[1]))
 from wrappers.WLED_wrapper import WLED
 
 # I don't think there's any way to backup the presets other than
-# going to http://192.168.1.121/edit and copying /presets.json
+# going to http://192.168.1.101/edit and copying /presets.json
 
 led = WLED()
 with open(Path(__file__).parent / "cfg.json", "w") as f:
