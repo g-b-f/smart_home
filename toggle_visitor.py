1dbed3fe8932e46438f2186e0b7c43c52e1daf18
whatbee <gabe.bf@gmail.com>
2026-03-20 12:40:34 +0000

    remove unused

--- toggle_visitor.py
+++
@@ -1,6 +0,0 @@
-from utils import mutable_globals
-
-present = mutable_globals.visitor_present
-mutable_globals.visitor_present = not present
-print(f"visitor_present was {present}, now {not present}")
-
