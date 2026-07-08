e097f05835c35a219e64acd682973404f068c609
whatbee <gabe.bf@gmail.com>
2026-07-08 18:01:41 +0100

    Make `periodic_light_check` connect to lights

--- periodic_tasks.py
+++ periodic_tasks.py
@@ -1,4 +1,6 @@
 from utils.misc import get_logger, get_zenith, lerp_color_temp, mutable_globals
+from wrappers.WLED_wrapper import WLED
+from wrappers.bulb_wrapper import Bulb
 
 logger = get_logger(__name__)
 
@@ -10,3 +12,10 @@ async def periodic_light_check():
     zen = get_zenith()
     logger.debug("Current zenith: %f", zen)
     temp = lerp_color_temp(zen)
+    logger.debug("Setting colourtemp to %d based on zenith", temp)
+
+    if mutable_globals.use_wled:
+        await WLED().turn_on(colortemp=temp)
+
+    if mutable_globals.use_bulb:
+        await Bulb().turn_on(colortemp=temp)
