45cbb5b8d072671594e2ecf98f38beea6ab83d48
whatbee <gabe.bf@gmail.com>
2026-07-13 17:15:35 +0100

    update teter.py

--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
 import asyncio
 import time
 
+from pywizlight import (  # type: ignore[import-untyped]
+    PilotBuilder,
+)
+
 import lighting_routines as Routine
 from utils.get_logger import get_logger
+from wrappers.bulb_wrapper import Bulb
 
 logger = get_logger(__name__, level="debug")
 
@@ -16,4 +21,18 @@ async def tester():
     logger.info("tracking stopped")
     await Routine.tracking_stopped()
 
-asyncio.run(tester())
+async def tester2():
+    bulb = Bulb()
+    light = bulb.light
+
+    # print(await light.getBulbConfig())
+    # print(await light.getModelConfig())
+    # print(await light.getUserConfig())
+    # print(await light.get_bulbtype())
+
+    builder = PilotBuilder(rgbww=(0, 0, 0, 0, 255))
+
+    await light.turn_on(builder)
+
+
+asyncio.run(tester2())
